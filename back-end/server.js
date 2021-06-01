94eb68bce1ac5c215c65651839dafdee77afd009
nathan <ndb.nathan.barrett@gmail.com>
2021-06-01 12:46:35 -0600

    1

---
+++ back-end/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+let items = [];
+let id = 0;
+
+app.post('/api/items', (req, res) => {
+  id = id + 1;
+  let item = {
+    id : id,
+    text: req.body.text,
+    completed: req.body.completed
+  };
+  items.push(item);
+  res.send(item);
+});
+
+app.get('/api/items', (req, res) => {
+  res.send(items);
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
